5020f7cef637bf92805dc868f467914cc716ee3a
driabwb <driabwb@gmail.com>
2022-05-06 00:50:21 -0600

    Be more judicious in error handling

--- sshconfig.go
+++ sshconfig.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io"
 )
 
 func addHost(ctx context.Context, writer io.Writer, hostName string) error {
-	writer.Write([]byte(fmt.Sprintf("Host %s", hostName)))
-	return errors.New("Blah")
+	_, err := writer.Write([]byte(fmt.Sprintf("Host %s", hostName)))
+	return err
 }
