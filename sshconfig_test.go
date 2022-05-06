c27b6f901bcfc4c15da1febb7d6310eccb439415
driabwb <driabwb@gmail.com>
2022-05-06 00:01:11 -0600

    Update test to make call work

--- sshconfig_test.go
+++ sshconfig_test.go
@@ -10,7 +10,7 @@ func TestWriteHost(t *testing.T) {
 	t.Run("Write a host line", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		addHost(context.Background(), buf, "TestHost")
+		addHost(context.Background(), &buf, "TestHost")
 
 		got := buf.String()
 		want := "Host TestHost"
