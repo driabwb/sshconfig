cd8be0d8bfd2c8adb4b28cf6eb8aa6e2c2bc1a30
driabwb <driabwb@gmail.com>
2022-05-06 00:47:19 -0600

    Returns an error

--- sshconfig.go
+++ sshconfig.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 )
 
-func addHost(ctx context.Context, writer io.Writer, hostName string) {
+func addHost(ctx context.Context, writer io.Writer, hostName string) error {
 	writer.Write([]byte(fmt.Sprintf("Host %s", hostName)))
+	return errors.New("Blah")
 }
