b52e186221d6be203f83098c48f454d410220878
driabwb <driabwb@gmail.com>
2022-05-06 00:45:17 -0600

    Add error check test

--- sshconfig_test.go
+++ sshconfig_test.go
@@ -3,8 +3,15 @@ package main
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"testing"
 )
+
+type badWriter struct{}
+
+func (b badWriter) Write(_ []byte) (int, error) {
+	return 0, fmt.Errorf("Bad Writer Error")
+}
 
 func TestWriteHost(t *testing.T) {
 	t.Run("Write TestHost host line", func(t *testing.T) {
@@ -31,4 +38,14 @@ func TestWriteHost(t *testing.T) {
 			t.Errorf("got %s; want %s", got, want)
 		}
 	})
+
+	t.Run("Return an error if writing fails", func(t *testing.T) {
+
+		got := addHost(context.Background(), badWriter{}, "BadWriterTest")
+
+		if nil == got {
+			t.Errorf("Expected an error got nil")
+		}
+	})
+
 }
