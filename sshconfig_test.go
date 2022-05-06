83d52260d2ca3c9aee8b519e44a4aaa153775817
driabwb <driabwb@gmail.com>
2022-05-05 23:55:57 -0600

    Add first host write test

---
+++ sshconfig_test.go
@@ -0,0 +1,23 @@
+package sshconfig
+
+import (
+	"bytes"
+	"context"
+	"testing"
+)
+
+func TestWriteHost(t *testing.T) {
+	t.Run("Write a host line", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		addHost(context.Background(), buf, "TestHost")
+
+		got := buf.String()
+		want := "Host TestHost"
+
+		if got != want {
+			t.Errorf("got %s; want %s", got, want)
+		}
+	})
+
+}
